equinox-platformx/react-native-health

feat(anchored-workout-query): create a workouts query using an hkanchoredobject
* Adds a library method that allows to query workouts via an HKAnchoredObjectQuery, exposing a query workouts results array and a resulting base-64 encoded anchor to the react-native bridge
* Adds utility methods to create an anchored query predicate with optional base-64 encoded anchor, startDate, and endDate params
* Adds documentations, as well as typescript definitions

MD-6255

RCTAppleHealthKit/RCTAppleHealthKit+Queries.m
@@ -232,6 +232,90 @@
     [self.healthStore executeQuery:query];
 }
 
+- (void)fetchAnchoredWorkouts:(HKSampleType *)type
+                         unit:(HKUnit *)unit
+                    predicate:(NSPredicate *)predicate
+                       anchor:(HKQueryAnchor *)anchor
+                        limit:(NSUInteger)lim
+                   completion:(void (^)(NSDictionary *, NSError *))completion {
+
+    // declare the block
+    void (^handlerBlock)(HKAnchoredObjectQuery *query, NSArray<__kindof HKSample *> *sampleObjects, NSArray<HKDeletedObject *> *deletedObjects, HKQueryAnchor *newAnchor, NSError *error);
+
+    // create and assign the block
+    handlerBlock = ^(HKAnchoredObjectQuery *query, NSArray<__kindof HKSample *> *sampleObjects, NSArray<HKDeletedObject *> *deletedObjects, HKQueryAnchor *newAnchor, NSError *error) {
+
+        if (!sampleObjects) {
+            if (completion) {
+                completion(nil, error);
+            }
+            return;
+        }
+
+        if (completion) {
+            NSMutableArray *data = [NSMutableArray arrayWithCapacity:1];
+
+            dispatch_async(dispatch_get_main_queue(), ^{
+                for (HKWorkout *sample in sampleObjects) {
+                    double energy =  [[sample totalEnergyBurned] doubleValueForUnit:[HKUnit kilocalorieUnit]];
+                    double distance = [[sample totalDistance] doubleValueForUnit:[HKUnit mileUnit]];
+                    NSString *type = [RCTAppleHealthKit stringForHKWorkoutActivityType:[sample workoutActivityType]];
+
+                    NSString *startDateString = [RCTAppleHealthKit buildISO8601StringFromDate:sample.startDate];
+                    NSString *endDateString = [RCTAppleHealthKit buildISO8601StringFromDate:sample.endDate];
+
+                    bool isTracked = true;
+                    if ([[sample metadata][HKMetadataKeyWasUserEntered] intValue] == 1) {
+                        isTracked = false;
+                    }
+
+                    NSString* device = @"";
+                    if (@available(iOS 11.0, *)) {
+                        device = [[sample sourceRevision] productType];
+                    } else {
+                        device = [[sample device] name];
+                        if (!device) {
+                            device = @"iPhone";
+                        }
+                    }
+
+                    NSDictionary *elem = @{
+                                           @"activityId" : [NSNumber numberWithInt:[sample workoutActivityType]],
+                                           @"HKWorkoutId" : [[sample UUID] UUIDString],
+                                           @"activityName" : type,
+                                           @"calories" : @(energy),
+                                           @"tracked" : @(isTracked),
+                                           @"metadata" : [sample metadata],
+                                           @"sourceName" : [[[sample sourceRevision] source] name],
+                                           @"sourceId" : [[[sample sourceRevision] source] bundleIdentifier],
+                                           @"device": device,
+                                           @"distance" : @(distance),
+                                           @"start" : startDateString,
+                                           @"end" : endDateString
+                                           };
+
+                    [data addObject:elem];
+                }
+                
+                NSData *anchorData = [NSKeyedArchiver archivedDataWithRootObject:newAnchor];
+                NSString *anchorString = [anchorData base64EncodedStringWithOptions:0];
+                completion(@{
+                            @"anchor": anchorString,
+                            @"data": data,
+                        }, error);
+            });
+        }
+    };
+
+    HKAnchoredObjectQuery *query = [[HKAnchoredObjectQuery alloc] initWithType:type
+                                                                     predicate:predicate
+                                                                        anchor:anchor
+                                                                         limit:lim
+                                                                resultsHandler:handlerBlock];
+
+    [self.healthStore executeQuery:query];
+}
+
 - (void)fetchSleepCategorySamplesForPredicate:(NSPredicate *)predicate
                                         limit:(NSUInteger)lim
                                    completion:(void (^)(NSArray *, NSError *))completion {

index.d.ts
@@ -91,6 +91,11 @@ declare module 'react-native-health' {
       callback: (err: string, results: Array<HealthValue>) => void,
     ): void
 
+    getAnchoredWorkouts(
+      options: HealthInputOptions,
+      callback: (err: string, results: AnchoredQueryResults) => void,
+    ): void
+
     getDailyStepCountSamples(
       options: HealthInputOptions,
       callback: (err: string, results: Array<HealthValue>) => void,
@@ -307,8 +312,24 @@ declare module 'react-native-health' {
     date?: string
     includeManuallyAdded?: boolean
     period?: number
+    anchor?: string;
   }
 
+  export interface HKWorkoutQueriedSampleType {
+    activityId: number
+    activityName: string
+    calories: number
+    device: string
+    HKWorkoutId: string
+    tracked: string
+    metadata: any
+    sourceName: string
+    sourceId: string
+    distance: number
+    start: string
+    end: string
+  }
+  
   export interface HealthValueOptions extends HealthUnitOptions {
     value: number
     startDate?: string
@@ -497,7 +518,7 @@ declare module 'react-native-health' {
     pound = 'pound',
     second = 'second',
   }
-  
+
   export enum HealthStatusCode {
     NotDetermined = 0,
     SharingDenied = 1,
@@ -509,6 +530,11 @@ declare module 'react-native-health' {
       read: HealthStatusCode[],
       write: HealthStatusCode[],
     }
+  }
+
+  export interface AnchoredQueryResults {
+    anchor: string,
+    data: Array<HKWorkoutQueriedSampleType>,
   }
 
   export enum HealthObserver {

sj19940817/health

RCTAppleHealthKit/RCTAppleHealthKit+Queries.m
@@ -230,6 +230,93 @@
     [self.healthStore executeQuery:query];
 }
 
+- (void)fetchAnchoredWorkouts:(HKSampleType *)type
+                    predicate:(NSPredicate *)predicate
+                       anchor:(HKQueryAnchor *)anchor
+                        limit:(NSUInteger)lim
+                   completion:(void (^)(NSDictionary *, NSError *))completion {
+
+    // declare the block
+    void (^handlerBlock)(HKAnchoredObjectQuery *query, NSArray<__kindof HKSample *> *sampleObjects, NSArray<HKDeletedObject *> *deletedObjects, HKQueryAnchor *newAnchor, NSError *error);
+
+    // create and assign the block
+    handlerBlock = ^(HKAnchoredObjectQuery *query, NSArray<__kindof HKSample *> *sampleObjects, NSArray<HKDeletedObject *> *deletedObjects, HKQueryAnchor *newAnchor, NSError *error) {
+
+        if (!sampleObjects) {
+            if (completion) {
+                completion(nil, error);
+            }
+            return;
+        }
+
+        if (completion) {
+            NSMutableArray *data = [NSMutableArray arrayWithCapacity:1];
+
+            dispatch_async(dispatch_get_main_queue(), ^{
+                for (HKWorkout *sample in sampleObjects) {
+                    @try {
+                        double energy =  [[sample totalEnergyBurned] doubleValueForUnit:[HKUnit kilocalorieUnit]];
+                        double distance = [[sample totalDistance] doubleValueForUnit:[HKUnit mileUnit]];
+                        NSString *type = [RCTAppleHealthKit stringForHKWorkoutActivityType:[sample workoutActivityType]];
+
+                        NSString *startDateString = [RCTAppleHealthKit buildISO8601StringFromDate:sample.startDate];
+                        NSString *endDateString = [RCTAppleHealthKit buildISO8601StringFromDate:sample.endDate];
+
+                        bool isTracked = true;
+                        if ([[sample metadata][HKMetadataKeyWasUserEntered] intValue] == 1) {
+                            isTracked = false;
+                        }
+
+                        NSString* device = @"";
+                        if (@available(iOS 11.0, *)) {
+                            device = [[sample sourceRevision] productType];
+                        } else {
+                            device = [[sample device] name];
+                            if (!device) {
+                                device = @"iPhone";
+                            }
+                        }
+
+                        NSDictionary *elem = @{
+                                               @"activityId" : [NSNumber numberWithInt:[sample workoutActivityType]],
+                                               @"id" : [[sample UUID] UUIDString],
+                                               @"activityName" : type,
+                                               @"calories" : @(energy),
+                                               @"tracked" : @(isTracked),
+                                               @"metadata" : [sample metadata],
+                                               @"sourceName" : [[[sample sourceRevision] source] name],
+                                               @"sourceId" : [[[sample sourceRevision] source] bundleIdentifier],
+                                               @"device": device,
+                                               @"distance" : @(distance),
+                                               @"start" : startDateString,
+                                               @"end" : endDateString
+                                               };
+
+                        [data addObject:elem];
+                    } @catch (NSException *exception) {
+                        NSLog(@"RNHealth: An error occured while trying to add workout sample: %@ ", [[[sample sourceRevision] source] bundleIdentifier]);
+                    }
+                }
+                
+                NSData *anchorData = [NSKeyedArchiver archivedDataWithRootObject:newAnchor];
+                NSString *anchorString = [anchorData base64EncodedStringWithOptions:0];
+                completion(@{
+                            @"anchor": anchorString,
+                            @"data": data,
+                        }, error);
+            });
+        }
+    };
+
+    HKAnchoredObjectQuery *query = [[HKAnchoredObjectQuery alloc] initWithType:type
+                                                                     predicate:predicate
+                                                                        anchor:anchor
+                                                                         limit:lim
+                                                                resultsHandler:handlerBlock];
+
+    [self.healthStore executeQuery:query];
+}
+
 - (void)fetchSleepCategorySamplesForPredicate:(NSPredicate *)predicate
                                         limit:(NSUInteger)lim
                                    completion:(void (^)(NSArray *, NSError *))completion {

index.d.ts
@@ -101,6 +101,11 @@ declare module 'react-native-health' {
       callback: (err: string, results: Array<HealthValue>) => void,
     ): void
 
+    getAnchoredWorkouts(
+      options: HealthInputOptions,
+      callback: (err: string, results: AnchoredQueryResults) => void,
+    ): void
+
     getDailyStepCountSamples(
       options: HealthInputOptions,
       callback: (err: string, results: Array<HealthValue>) => void,
@@ -347,8 +352,24 @@ declare module 'react-native-health' {
     date?: string
     includeManuallyAdded?: boolean
     period?: number
+    anchor?: string;
   }
 
+  export interface HKWorkoutQueriedSampleType {
+    activityId: number
+    activityName: string
+    calories: number
+    device: string
+    id: string
+    tracked: string
+    metadata: any
+    sourceName: string
+    sourceId: string
+    distance: number
+    start: string
+    end: string
+  }
+  
   export interface HealthValueOptions extends HealthUnitOptions {
     value: number
     startDate?: string
@@ -557,6 +578,11 @@ declare module 'react-native-health' {
       read: HealthStatusCode[]
       write: HealthStatusCode[]
     }
+  }
+
+  export interface AnchoredQueryResults {
+    anchor: string,
+    data: Array<HKWorkoutQueriedSampleType>,
   }
 
   export enum HealthObserver {