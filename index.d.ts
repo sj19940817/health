equinox-platformx/react-native-health

fix(types): fix getAnchoredWorkouts error type
Function callback was typing the error param as string,
but the actual error is an object.

MD-11136

index.d.ts
@@ -13,6 +13,10 @@ declare module 'react-native-health' {
     Units: Record<HealthUnit, HealthUnit>
   }
 
+  export interface FetchHKError {
+    message?: string;
+  }
+
   export interface AppleHealthKit {
     initHealthKit(
       permissions: HealthKitPermissions,
@@ -93,7 +97,7 @@ declare module 'react-native-health' {
 
     getAnchoredWorkouts(
       options: HealthInputOptions,
-      callback: (err: string, results: AnchoredQueryResults) => void,
+      callback: (err: FetchHKError, results: AnchoredQueryResults) => void,
     ): void
 
     getDailyStepCountSamples(

sj19940817/health

index.d.ts
@@ -15,6 +15,10 @@ declare module 'react-native-health' {
     Units: Record<HealthUnit, HealthUnit>
   }
 
+  export interface HKErrorResponse {
+    message?: string;
+  }
+
   export interface AppleHealthKit {
     initHealthKit(
       permissions: HealthKitPermissions,
@@ -120,7 +124,7 @@ declare module 'react-native-health' {
 
     getAnchoredWorkouts(
       options: HealthInputOptions,
-      callback: (err: string, results: AnchoredQueryResults) => void,
+      callback: (err: HKErrorResponse, results: AnchoredQueryResults) => void,
     ): void
 
     getDailyStepCountSamples(